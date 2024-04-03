92bdfbb9226adb8f07288da790ad1153de4fed23
Ramil <ramig.1985@gmail.com>
2024-04-03 18:31:02 +0000

    Add slider and video player

--- src/components/TourMedia.vue
+++ src/components/TourMedia.vue
@@ -1,9 +1,91 @@
 <script setup>
+  import 'vue3-carousel/dist/carousel.css'
+  import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+  import VuePlyr from 'vue-plyr'
+  import 'vue-plyr/dist/vue-plyr.css'
+
   const props = defineProps({
     tour: Object
   })
 </script>
 
 <template>
-  <div>Media</div>
+  <div class="tour-preview__carousel">
+    <Carousel :wrap-around="true" :breakpoints="breakpoints" :items-to-show="1.2">
+      <Slide :key="tour.video_url">
+        <vue-plyr>
+          <div class="plyr__video-embed">
+            <iframe :src="tour.video_url" allowfullscreen allowtransparency allow="autoplay"></iframe>
+          </div>
+        </vue-plyr>
+      </Slide>
+
+      <Slide v-for="image_url in tour.image_urls" :key="image_url">
+        <div class="tour-preview__carousel-image-wrapper">
+          <img v-bind:src="image_url"></img>
+        </div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </div>
 </template>
+
+<style>
+  .tour-preview__carousel {
+    .tour-preview__carousel-image-wrapper {
+      height: 400px;
+      min-width: 100%;
+      background-color: var(--vc-clr-primary);
+      color: var(--vc-clr-white);
+      border-radius: 8px;
+
+      img {
+        height: 400px;
+        min-width: 100%;
+        object-fit: cover;
+      }
+    }
+
+    .carousel__slide {
+      padding: 0.4rem;
+    }
+
+    .carousel__prev,
+    .carousel__next {
+      background-color: var(--vt-c-white);
+      border-radius: 1rem;
+      font-size: 1rem;
+      margin: 0 10%;
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+  }
+
+  .plyr {
+    height: 400px;
+    min-width: 100%;
+  }
+
+  .plyr__video-embed {
+    min-height: 100%;
+  }
+
+  .plyr:not(:fullscreen) {
+    .plyr__video-embed {
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+
+  @media (max-width: 800px) {
+    .tour-preview__carousel .carousel__slide {
+      padding: 0;
+      margin-right: -1px;
+    }
+  }
+</style>
--- src/components/TourPage.vue
+++ src/components/TourPage.vue
@@ -3,7 +3,15 @@
   import TourMedia from './TourMedia.vue'
   import TourDescription from './TourDescription.vue'
 
-  const tour = ref({})
+  const tour = ref({
+    "video_url": "https://www.youtube.com/watch?v=_166VGD6kLs",
+    "image_urls": [
+      "https://images.pexels.com/photos/1842332/pexels-photo-1842332.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/5414577/pexels-photo-5414577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/11777903/pexels-photo-11777903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/12936034/pexels-photo-12936034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    ]
+  })
 </script>
 
 <template>
